c1901173852c5804390117cb8a54359a2f87563b
Md. Ariful Islam <76257163+arifulislamarman@users.noreply.github.com>
2021-09-16 00:55:59 +0600

    Add files via upload

---
+++ website/auth.py
@@ -0,0 +1,62 @@
+from website import views
+from flask import Blueprint, render_template, request, flash,redirect,url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from .import db
+from flask_login import login_user, login_required, logout_user,current_user
+
+
+auth = Blueprint('auth',__name__)
+
+@auth.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in Successfully!', category='success')
+                login_user(user,remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect Passwrod, Try Again.', category='error')
+        else:
+            flash('Email Does Not Exist!', category='error')
+            
+    return render_template("login.html", user=current_user)
+
+@auth.route('/logout')
+@login_required  #this call decerotor
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
+
+@auth.route('/singup', methods=['GET','POST'])
+def singup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('first_name')
+        #last_name = request.form.get('last_name')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email Already Exist!. Try Different One', category='error')
+        elif len(email) < 4 :
+            flash('Email must be greather then 3 charecters', category='error')
+        elif len(first_name ) < 2 :
+            flash('First Name must be greather then 1 Characters', category='error')
+        # elif len(last_name ) < 2 :
+        #     flash('Last Name must be greather then 1 Characters', category='error')
+        elif password1 != password2 :
+            flash('Password Don\'t Match', category='error')
+        else:
+            new_user = User(email=email,password=generate_password_hash(password1, method='sha256'),first_name=first_name)
+            db.session.add(new_user)
+            db.session.commit() 
+            login_user(user,remember=True)
+            flash('Account Created!', category='success')
+            return redirect(url_for('views.home'))
+
+    return render_template("sign_up.html",user=current_user)
